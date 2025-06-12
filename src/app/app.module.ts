a4a3dd0627934802c68a3a58793dd751d190f75a
NikhilCharan-3 <Bellam.NikhlCharan@cognizant.com>
2025-06-12 11:48:47 +0530

    added global error & http error Handlers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { JwtInterceptor } from './modules/login/services/jwt.interceptor';
-
+import { GlobalErrorHandlerService } from './global-error-handler-exception.service';
+import { HttpErrorInterceptor } from './http-error.interceptor';
 @NgModule({
   declarations: [AppComponent],
 
@@ -15,6 +17,12 @@ import { JwtInterceptor } from './modules/login/services/jwt.interceptor';
       useClass: JwtInterceptor,
       multi: true,
     },
+    { provide: ErrorHandler, 
+      useClass: GlobalErrorHandlerService 
+    },
+    { provide: HTTP_INTERCEPTORS, 
+      useClass: HttpErrorInterceptor, multi: true 
+    }
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/http-error.interceptor.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error: HttpErrorResponse) => {
+        console.error('HTTP error occurred:', error);
+        alert('An error occurred while processing your request.');
+        return throwError(() => error);
+      })
+    );
+  }
+}
