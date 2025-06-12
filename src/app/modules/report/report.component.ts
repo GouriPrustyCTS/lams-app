d80f3fab140067d68241b7255aa92f87a921b568
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-06-12 17:02:41 +0530

    99 % done

--- src/app/modules/report/report.component.ts
+++ src/app/modules/report/report.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../login/services/auth.service';
 
 @Component({
   selector: 'report-root',
   standalone: false,
   templateUrl: './report.component.html',
-  styleUrl: './report.component.css'
+  styleUrl: './report.component.css',
 })
 export class ReportComponent {
   title = 'lams-reports-ui';
+  employeeId!: number;
+  fetch(): void {
+    const employeeId = Number(
+      this.authService.getDetailsFromToken(this.authService.getToken())
+        .employeeId
+    ); // Assuming you store employeeId on login
+    this.employeeId = employeeId;
+    console.log(employeeId);
+  }
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {
+    this.fetch();
+  }
 }
